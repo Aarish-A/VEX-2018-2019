9430b19bae4f1cb42db1530224e467d2b624af88
anjaleenaren <31371146+anjaleenaren@users.noreply.github.com>
2018-09-08 02:00:20 -0400

    Cleaned up mastercode

--- src/controls.h
+++ src/controls.h
@@ -2,17 +2,5 @@
 #define JOY_THROTTLE Ch3
 #define JOY_TURN Ch4
 
-/* Lift */
-#define JOY_LIFT CH2
-
-/* Arm */
-#define JOY_ARM CH1
-
-/* Mobile Goal */
-#define BTN_MOBILE_TOGGLE Btn6U
-#define BTN_MOBILE_MIDDLE Btn6D
-
-/* Macros */
-#define BTN_STACK Btn8D
-#define BTN_LIFT_TEST Btn7U
-#define BTN_DRIVE_TEST Btn7D
+/* Puncher */
+#define BTN_SHOOT Btn6U
