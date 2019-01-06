37d1d86a625f22914b01d86eb3b7c4377e4e93d3
Nicholas Mertin <nickmertin@gmail.com>
2019-01-06 18:58:45 -0500

    Change value retrieval logic in settings manager

--- main/src/settings.cpp
+++ main/src/settings.cpp
@@ -6,7 +6,8 @@ using namespace std;
 namespace {
   class SettingValue {
   public:
-    virtual ~SettingValue() = default;
+    virtual int32_t get_int(int32_t default_value) = 0;
+    virtual double get_double(double default_value) = 0;
   };
 
   class IntSettingValue final : public SettingValue {
@@ -14,6 +15,14 @@ namespace {
     int32_t value;
 
     IntSettingValue(int32_t value) : value(value) {}
+
+    int32_t get_int(int32_t default_value) override {
+      return value;
+    }
+
+    double get_double(double default_value) override {
+      return value;
+    }
   };
 
   class DoubleSettingValue final : public SettingValue {
@@ -21,6 +30,14 @@ namespace {
     double value;
 
     DoubleSettingValue(double value) : value(value) {}
+
+    int32_t get_int(int32_t default_value) override {
+      return static_cast<int32_t>(value);
+    }
+
+    double get_double(double default_value) override {
+      return value;
+    }
   };
 
   map<string, SettingValue *> settings_map;
@@ -32,38 +49,19 @@ namespace piln::settings {
 
   int32_t get_int(const std::string &key, int32_t default_value) {
     auto iter = settings_map.find(key);
-    if (iter == settings_map.end()) {
-      settings_map[key] = new IntSettingValue(default_value);
-      return default_value;
-    }
-    auto value = dynamic_cast<IntSettingValue *>(iter->second);
-    if (value) return value->value;
-    else {
-      delete iter->second;
-      iter->second = new IntSettingValue(default_value);
-      return default_value;
-    }
+    return iter == settings_map.end() ? default_value : iter->second->get_int(default_value);
   }
 
   double get_double(const std::string &key, double default_value) {
     auto iter = settings_map.find(key);
-    if (iter == settings_map.end()) {
-      settings_map[key] = new DoubleSettingValue(default_value);
-      return default_value;
-    }
-    auto value = dynamic_cast<DoubleSettingValue *>(iter->second);
-    if (value) return value->value;
-    else {
-      delete iter->second;
-      iter->second = new DoubleSettingValue(default_value);
-      return default_value;
-    }
+    return iter == settings_map.end() ? default_value : iter->second->get_double(default_value);
   }
 
   void set_int(const std::string &key, int32_t value) {
     auto iter = settings_map.find(key);
     if (iter == settings_map.end()) {
       settings_map[key] = new IntSettingValue(value);
+      return;
     }
     auto ptr = dynamic_cast<IntSettingValue *>(iter->second);
     if (ptr) {
@@ -79,6 +77,7 @@ namespace piln::settings {
     auto iter = settings_map.find(key);
     if (iter == settings_map.end()) {
       settings_map[key] = new DoubleSettingValue(value);
+      return;
     }
     auto ptr = dynamic_cast<DoubleSettingValue *>(iter->second);
     if (ptr) {
