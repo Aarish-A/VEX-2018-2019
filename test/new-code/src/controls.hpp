c56383e9f584ab659427e95e75266ef567d1666e
Aarish Adeel <aarish8@gmail.com>
2019-01-19 13:42:41 -0500

    Fixed single button presses

--- test/new-code/src/controls.hpp
+++ test/new-code/src/controls.hpp
@@ -1,8 +1,10 @@
 #pragma once
 #include "main.h"
 
-constexpr pros::controller_digital_e_t DP_R1 = pros::E_CONTROLLER_DIGITAL_R1;
-constexpr pros::controller_digital_e_t DP_L1 = pros::E_CONTROLLER_DIGITAL_L1;
+constexpr int GET_BTN_INDEX(pros::controller_digital_e_t btn) { return (btn - pros::E_CONTROLLER_DIGITAL_L1); }
 
-constexpr pros::controller_digital_e_t SP_A = pros::E_CONTROLLER_DIGITAL_A;
-constexpr pros::controller_digital_e_t FE_X = pros::E_CONTROLLER_DIGITAL_X;
+constexpr int DP_R1 = GET_BTN_INDEX(pros::E_CONTROLLER_DIGITAL_R1);
+constexpr int DP_L1 = GET_BTN_INDEX(pros::E_CONTROLLER_DIGITAL_L1);
+
+constexpr int SP_A = GET_BTN_INDEX(pros::E_CONTROLLER_DIGITAL_A);
+constexpr int FE_X = GET_BTN_INDEX(pros::E_CONTROLLER_DIGITAL_X);
--- test/new-code/src/opcontrol.cpp
+++ test/new-code/src/opcontrol.cpp
@@ -11,11 +11,12 @@ void opcontrol() {
 	while (true) {
 		update_buttons();
 
+		printf("R1: %d C: %d L: %d T: %d %d\n", pros::millis(), (int)(buttons[DP_R1].pressed), (int)(buttons[DP_R1].last_pressed), buttons[DP_R1].last_pressed_time, (int)check_rising(DP_R1));
+		printf("L1: %d C: %d L: %d T: %d %d\n", pros::millis(), (int)(buttons[DP_L1].pressed), (int)(buttons[DP_L1].last_pressed), buttons[DP_L1].last_pressed_time, (int)check_rising(DP_L1));
 
-		if (check_double_press(DP_R1, DP_L1)) printf("DOUBLE PRESSED\n"); //ctrler.print(2, 0, "DOUBLE PRESS   ");
-		else if (check_single_press(SP_A)) printf("SINGLE PRESSED\n"); //ctrler.print(2, 0, "SINGLE PRESS   ");
-		else if (check_falling(FE_X)) printf("FALLING EDGED\n"); //ctrler.print(2, 0, "FALLING EDGE    ");
-
+		// printf("%d C: %d L: %d T: %d %d\n", pros::millis(), (int)(buttons[SP_A].pressed), (int)(buttons[SP_A].last_pressed), buttons[SP_A].last_pressed_time, (int)check_rising(SP_A));
+		if (check_double_press(DP_R1, DP_L1)) printf("%d DOUBLE PRESSED\n", millis()); //ctrler.print(2, 0, "DOUBLE PRESS   ");
+		else if (check_single_press(SP_A)) printf("%d SINGLE PRESSED\n", millis()); //ctrler.print(2, 0, "SINGLE PRESS   ");
 
 		delay(10);
 	}
