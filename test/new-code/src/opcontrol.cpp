12f55403bd0f88cb42d268a1c6d08bede7ba35fc
Aarish Adeel <aarish8@gmail.com>
2019-01-19 14:21:35 -0500

    Added back and cleaned up logs

--- test/new-code/src/opcontrol.cpp
+++ test/new-code/src/opcontrol.cpp
@@ -11,8 +11,9 @@ void opcontrol() {
 	while (true) {
 		update_buttons();
 
-		printf("R1: %d C: %d L: %d T: %d %d\n", pros::millis(), (int)(buttons[DP_R1].pressed), (int)(buttons[DP_R1].last_pressed), buttons[DP_R1].last_pressed_time, (int)check_rising(DP_R1));
+		printf("R1: %d C: %d L: %d T: %d %d %d\n", pros::millis(), (int)(buttons[DP_R1].pressed), (int)(buttons[DP_R1].last_pressed), buttons[DP_R1].last_pressed_time, (int)check_rising(DP_R1), buttons[DP_R1].button_press_time);
 		printf("L1: %d C: %d L: %d T: %d %d\n", pros::millis(), (int)(buttons[DP_L1].pressed), (int)(buttons[DP_L1].last_pressed), buttons[DP_L1].last_pressed_time, (int)check_rising(DP_L1));
+
 
 		// printf("%d C: %d L: %d T: %d %d\n", pros::millis(), (int)(buttons[SP_A].pressed), (int)(buttons[SP_A].last_pressed), buttons[SP_A].last_pressed_time, (int)check_rising(SP_A));
 		if (check_double_press(DP_R1, DP_L1)) printf("%d DOUBLE PRESSED\n", millis()); //ctrler.print(2, 0, "DOUBLE PRESS   ");
