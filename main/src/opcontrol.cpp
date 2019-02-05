d6aab7f1db1e7b3a70bcdfa988479c353e6e64e2
Aarish Adeel <aarish8@gmail.com>
2019-02-05 12:04:19 -0500

    Removed controller printing from main opcontrol loop (kept it inside update_controller_lcd function)

--- main/src/opcontrol.cpp
+++ main/src/opcontrol.cpp
@@ -51,39 +51,6 @@ void opcontrol() {
 		//printf("%d\n", (int)decapper.get_position());
 		//log_ln(LOG_SHOTS, "  >>> %d PF BACK: %d %d | l_counter:%d", pros::millis(), pf_back_SP.top, pf_back_SP.mid, loop_counter);
 		//printf("  >>> %d PRINT - PF BACK: %d %d | l_counter:%d \n", pros::millis(), pf_back_SP.top, pf_back_SP.mid, loop_counter);
-
-		if (millis() - print_time > 30) {
-			print_time = millis();
-			std::string field_pos_s = "def";
-			FieldPos field_pos= shot_req[0].field_pos;
-
-			if (field_pos== FieldPos_Front) field_pos_s = "Fr ";
-			else if (field_pos== FieldPos_Back) field_pos_s = "Bck";
-			else if (field_pos== FieldPos_PF) field_pos_s = "PF ";
-			else if (field_pos== FieldPos_PF_Back_Red) field_pos_s = "PfR";
-			else if (field_pos== FieldPos_PF_Back_Blue) field_pos_s = "PfB";
-
-			std::string team_s = blue_team? "b" : "r";
-			//ctrler.print(2, 0, "%d %d%d%d%d%d%d      ", field_pos_s, team_s, (int)angler.get_temperature(), (int)puncherLeft.get_temperature(), (int)puncherRight.get_temperature(), (int)drive_fl.get_temperature(), (int)drive_fr.get_temperature(), (int)drive_bl.get_temperature(), (int)drive_br.get_temperature());
-			//ctrler.print(2, 0, "BD: %d   ", ball_sensor.get_value());
-			// eff_fl = drive_fl.get_efficiency();
-			// eff_bl = drive_bl.get_efficiency();
-			// eff_fr = drive_fr.get_efficiency();
-			// eff_br = drive_br.get_efficiency();
-
-			/*if (eff_fl < EFF_LIM || eff_fr < EFF_LIM || eff_bl < EFF_LIM || eff_br < EFF_LIM)
-				ctrler.rumble(". .");
-				*/
-
-			// ctrler.print(2, 0, "%d,%d,%d,%d", (int)drive_bl.get_temperature(), (int)drive_br.get_temperature(), (int)drive_fl.get_temperature(), (int)drive_fr.get_temperature());
-			// ctrler.print(2, 0, "%s %.1f %.1f %.1f %.1f    ", field_pos_s, eff_fl, eff_bl, eff_fr, eff_br);
-			//ctrler.print(2, 0, "%s %s %d %d %d      ", field_pos_s, team_s, (int)intake.get_temperature(), (int)puncherLeft.get_temperature(), (int)puncherRight.get_temperature());
-			// ctrler.print(2, 0, "%d  ", (int)current_auto_routine);
-			ctrler.print(2, 0, "a: %.1f          ", RAD_TO_DEG(getGlobalAngle()));
-			//ctrler.print(2, 0, "%d %d %.1f     ", enc_l.get_value(), enc_r.get_value(), RAD_TO_DEG(pos.a));
-			//ctrler.print(2, 0, "%f          ", ((enc_l.get_value() * SPN_TO_IN_L) - (enc_r.get_value() * SPN_TO_IN_R)) / 3600_deg);
-
-		}
 		//printf("%d, %f to %f \n", pros::millis(), puncherLeft.get_position(), PUN_OFFSET + (1 * PUN_TPR) - PUN_BALL_CHK_START[1]);
 		loop_counter++;
 		delay(10);
