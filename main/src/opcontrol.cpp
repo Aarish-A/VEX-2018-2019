5b49d58fd31d0d1a734e9bb06efbb719ea04cf00
anjalee-narenthiren <42633976+anjalee-narenthiren@users.noreply.github.com>
2019-03-12 11:53:26 -0400

    strauss suggestions

--- main/src/opcontrol.cpp
+++ main/src/opcontrol.cpp
@@ -14,9 +14,10 @@ void opcontrol() {
 	while(true)
 	{
 		while (log_file == NULL) {
+			//printf("  %d opeEN | errno: %d %s| %ld | %p\n", pros::millis(), errno, strerror(errno),  counter, log_file);
 			log_file = fopen(log_file_name, log_mode);
 			if (log_file == NULL || errno != 0) {
-				printf(" ERR %d log_file fopen failed | errno: %d | %ld | %p\n", pros::millis(), errno, counter, log_file);
+				printf(" ERR %d log_file fopen failed | errno: %d %s| %ld | %p\n", pros::millis(), errno, strerror(errno), counter, log_file);
 				pros::delay(1);
 			} //else printf("\n       >>> %d OPEN LOG_FILE: %p | %ld \n", pros::millis(), log_file, counter);
 		}
@@ -27,24 +28,27 @@ void opcontrol() {
 			if (flush_amount > 0 || !ferror(log_file))  {
 				//printf("   >>> %d fwrite LOG_FILE: %p | Ferror:%d | errno: %d | %ld \n", pros::millis(), log_file, ferror(log_file), errno, counter);
 			} else {
-				if (flush_amount <= 0) printf("\n%d Flushed 1FAIL %d | Ferror:%d | errno: %d | %p\n", pros::millis(), flush_amount, ferror(log_file), errno, log_file);
+				if (flush_amount <= 0) printf("\n%d Flushed 1FAIL %d | Ferror:%d | errno: %d %s | %p\n", pros::millis(), flush_amount, ferror(log_file), errno, strerror(errno), log_file);
 				if (ferror(log_file)) {
-					printf("\n%d Flushed 2FAIL %d | Ferror:%d | errno: %d | %p\n", pros::millis(), flush_amount, ferror(log_file), errno, log_file);
+					printf("\n%d Flushed 2FAIL %d | Ferror:%d | errno: %d %s | %p\n", pros::millis(), flush_amount, ferror(log_file), errno, strerror(errno), log_file);
 				}
 			}
 		//pros::delay(10);
 		//close
-
-			int f_close_ret = -1;
+		int f_close_ret = -1;
+		while (f_close_ret != 0)
+		{
 			f_close_ret = fclose(log_file);
 			if (f_close_ret != 0) {
-				printf("  %d ERR log_file fclose failed | errno: %d | f_close_ret: %d | %ld | %p\n", pros::millis(), errno, f_close_ret, counter, log_file);
+				printf("  %d ERR log_file fclose failed | errno: %d | f_close_ret: %d | %ld\n", pros::millis(), errno, f_close_ret, counter);
 			}
 		 else {
 			 log_file = NULL; // Set log_file to null upon successful close
-			 //printf("    >>> %d CLOSELOG_FILE: %p | %ld \n", pros::millis(), log_file, counter);
+			 printf("    >>> %d CLOSELOG_FILE: %p | %ld \n", pros::millis(), log_file, counter);
 		 }
-		}
+	 }
+			//pros::delay(10);
+	}
 		//pros::delay(100);
 		counter++;
 	}
