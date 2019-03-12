e648f068a91cca23189ec46d9a8c0067ae604808
anjalee-narenthiren <42633976+anjalee-narenthiren@users.noreply.github.com>
2019-03-11 21:39:43 -0400

    Clean up logging

--- main/src/libraries/logs.cpp
+++ main/src/libraries/logs.cpp
@@ -4,41 +4,25 @@ pros::Mutex sd_buffer_mutex;
 char log_buffer[LOG_BUFFER_SIZE];
 int buffer_write_index = 0;
 int buffer_flush_index = 0;
-int buffer_head = 0;
-int buffer_tail = 0;
+
 
 FILE* log_file = NULL;
 
 const char* log_file_name = "/usd/log.txt";
 const char* const log_mode = "a";
 
-void log_init() {
-  printf(" %d log_init(): Start Logging for Program \n", pros::millis());
+/* Open and Close Log File */
+void open_log_file() {
   int open_attempt_count = 0;
   while (log_file == NULL && open_attempt_count < MAX_ALLOWABLE_OPEN_ATTEMPTS) {
     log_file = fopen(log_file_name, log_mode);
-    printf("     %d log_init() FILE-OPEN ATTEMPT %d\n", pros::millis(), open_attempt_count);
-    open_attempt_count++;
-    pros::delay(1);
+    if (log_file == NULL) {
+			printf(" ERR %d log_file fopen failed | errno: %d | file-open-attempt: %d \n", pros::millis(), errno, open_attempt_count);
+      open_attempt_count++;
+      pros::delay(1);
+		} //else printf("\n\n\n       >>> %d OPEN LOG_FILE: %p   \n\n\n", pros::millis(), log_file);
+
   }
-	if (log_file == NULL) {
-    sd_logging_enabled = false;
-		printf("  >>>> %d COULD NOT OPEN SD LOG FILE \n", pros::millis());
-	}
-  else {
-    sd_logging_enabled = true;
-    printf(">>>> %d log_init(): Successfully opened SD log file \n", pros::millis());
-  	fputs("\r\n\r\n--------------------------------------------------\r\n\r\n", log_file);
-    fprintf(log_file, ">>>> %d Start Logging for Program \n", pros::millis());
-    close_log_file();
-  }
-  pros::delay(50);
-  if (sd_logging_enabled) {
-    log_ln(PROGRAM_FLOW, " Construct buffer_to_sd() task from log_init()");
-    pros::Task buffer_to_sd_task((pros::task_fn_t) buffer_to_sd);
-  }
-  pros::delay(50);
-  log_ln(PROGRAM_FLOW, "DONE LOG_INIT");
 }
 
 void close_log_file() {
@@ -47,33 +31,55 @@ void close_log_file() {
     int attempt_cnt  = 0;
     while (f_close_ret != 0 && attempt_cnt < MAX_ALLOWABLE_CLOSE_ATTEMPTS) {
       f_close_ret = fclose(log_file);
-      attempt_cnt++;
-      pros::delay(1);
+      if (f_close_ret != 0) {
+        printf("  %d ERR log_file fclose failed | errno: %d | f_close_ret: %d | attempt-cnt: %d | \n", pros::millis(), errno, f_close_ret, attempt_cnt);
+        attempt_cnt++;
+        pros::delay(1);
+      }
     }
-    if (f_close_ret == 0) log_file = NULL;
-    // ELSE KILL BUFF_SD TASK IN FEAR OF MAKING CODE HANG
-    printf(" \n \n%d FClose: %d took %d tries\n \n", pros::millis(), f_close_ret, attempt_cnt);
+    if (f_close_ret == 0) log_file = NULL; // Set log_file to null upon successful close
   }
 }
 
-void log_ln_internal(const char * str_whole) {
+/* Logging Functions */
+void log_init() {
+  printf(" %d log_init(): Start Logging for Program \n", pros::millis());
+  open_log_file(); // Open log_file
+	if (log_file == NULL) { // If the log_file could not be oppend
+    sd_logging_enabled = false;
+		printf("  >>>> %d COULD NOT OPEN SD LOG FILE \n", pros::millis());
+	}
+  else { // If the log_file could be oppened
+    sd_logging_enabled = true;
+    printf(">>>> %d log_init(): Successfully opened SD log file \n", pros::millis());
+  	fputs("\r\n\r\n--------------------------------------------------\r\n\r\n", log_file);
+    fprintf(log_file, ">>>> %d Start Logging for Program \n", pros::millis());
+    close_log_file();
+  }
+  pros::delay(10);
+  if (sd_logging_enabled) { // Start buffer -> sd task
+    log_ln(PROGRAM_FLOW, " Construct buffer_to_sd() task from log_init()");
+    pros::Task buffer_to_sd_task((pros::task_fn_t) buffer_to_sd);
+  }
+  pros::delay(50); // Give the task time to start up (it is not started instantly)
+  log_ln(PROGRAM_FLOW, "DONE LOG_INIT");
+}
 
-  // Write to console
+void log_ln_internal(const char * str_whole) {
+  // 1) Write to console
   printf("%s",str_whole);
 
+  // 2) Write to sd
   if (sd_logging_enabled)
   {
-    // Write to sd_logging buffer
     size_t str_whole_len = strlen(str_whole); // Amount of chars in input-string
     int write_amount = 0;
     if (sd_buffer_mutex.take(LOG_MUTEX_TO))
     {
-      //printf("        >>%d LOG_LN() TOOK sd_buffer_mutex \n", pros::millis());
       size_t buf_remaining_len = LOG_BUFFER_SIZE - buffer_write_index; // Amount of unfilled indices left in buffer
       if (buf_remaining_len < str_whole_len) { // If the input-string is about to go out of the bounds of the buffer
                                                 // Split the input-string in two. Put whatever fits into the end of the buffer
                                                 // and put the remaining characters into the front of the buffer
-        //printf(">>%d log_ln() WRITE->BUFFER wrapped around \n", pros::millis());
         // Split whole string into two substrings
         const size_t str_first_len = buf_remaining_len;
         const size_t str_sec_len = str_whole_len-str_first_len;
@@ -92,94 +98,18 @@ void log_ln_internal(const char * str_whole) {
         //printf("  >>%d log_ln() | write_amount = %d | %d %d \n", pros::millis(), write_amount, buffer_write_index, buffer_flush_index);
       }
       else {
-        //printf("  >>%d log_ln() WRITE->BUFFER good around \n", pros::millis());
         write_amount = sprintf(log_buffer+BUF_OBJ_SIZE*buffer_write_index, "%s", str_whole);
-        if (write_amount > 0) {
-          buffer_write_index += write_amount;
-        }
+        if (write_amount > 0) buffer_write_index += write_amount;
       }
-      //printf("    %d buffer: %d \n", pros::millis(), buffer_write_index);
       sd_buffer_mutex.give();
       //printf("        >>%d LOG_LN() GAVE sd_buffer_mutex \n", pros::millis());
-
-    } else printf(">>> %d log_ln_internal() sd_buffer_mutex take failed (TO = 50ms) | Err:%d \n", pros::millis(), errno);
-  }
-
-}
-
-void flush_to_sd(int given_amnt_to_flush) {
-  //Open file
-  while (log_file == NULL) {
-    log_file = fopen(log_file_name, log_mode);
-    //printf("  \n>>>>>>>>%d Opening file \n\n", pros::millis());
-    pros::delay(2);
-  }
-  //printf("                  \n  >>>%d Opened file <<< \n\n", pros::millis());
-
-  int amnt_to_flush = std::min(given_amnt_to_flush, MAX_AMNT_TO_FLUSH); // Lim num of indices to flush to MAX_AMNT_TO_FLUSH
-
-  const char* FLUSH_START_POS = &log_buffer[buffer_flush_index];
-  //printf("%d >>>before ToFLUSH: %d | %d %d | \n", pros::millis(), amnt_to_flush, buffer_write_index, buffer_flush_index);
-  int flush_amount = fwrite(FLUSH_START_POS, BUF_OBJ_SIZE, amnt_to_flush, log_file);
-  //printf("%d >>>after FLUSH %d | %d %d | err: %d\n", pros::millis(), flush_amount,  buffer_write_index, buffer_flush_index, ferror(log_file));
-  int old_bfi = buffer_flush_index;
-  if (flush_amount >  0) buffer_flush_index += flush_amount;
-  if (buffer_flush_index >= LOG_BUFFER_SIZE) {
-    buffer_flush_index = 0; // If the pointer is at the end of the buffer, move it back to the front
-    //printf("    >>%d BFI OVERFLOW -> Move to %d \n", pros::millis(), buffer_flush_index);
-  }
-  //printf("%d flush | count: %d | %d %d | old:%d acc:%d\n", pros::millis(), flush_amount, buffer_write_index, buffer_flush_index, old_bfi, old_bfi+flush_amount);
-  close_log_file();
-}
-
-void buffer_to_sd() {
-  printf("  %d PRINT - buffer start \n", pros::millis());
-  log_ln(PROGRAM_FLOW, "%d Start buffer_to_sd() task \n", pros::millis());
-  while (true) {
-    if (sd_buffer_mutex.take(TIMEOUT_MAX))
-    {
-      int count = buffer_write_index - buffer_flush_index;
-      if (count > 0) { // The last character flushed from buf is behind last character written to it
-        // Flush everything b/w buffer_flush_index and buffer_write_index
-        flush_to_sd(count);
-      }
-      else if (count < 0) { // We wrapped around when writing to the buffer (meaning the last character written is behind the last character flushed)
-        printf("  >>%d buffer_to_sd() BUFFER->SD wrapped around \n", pros::millis());
-        // Flush b/w buffer_flush_index & buffer_end. Move the pointer back to the beginning of the buffer
-        int buf_remaining_len = LOG_BUFFER_SIZE - buffer_flush_index; // Amount of unflushed indices left in buffer
-        flush_to_sd(buf_remaining_len);
-      }
-
-      sd_buffer_mutex.give();
-
-    } else printf(">>> %d buffer_to_sd() sd_buffer_mutex Take Failed (TO = TIMEOUT_MAX) | Err:%d \n", pros::millis(), errno);
-
-    pros::delay(LOG_BUFFER_FLUSH_DELAY);
+    } else printf("\n   ERR>>> %d log_ln_internal() sd_buffer_mutex take failed (TO = 50ms) | Err:%d \n\n", pros::millis(), errno);
   }
 }
-
-std::string string_format(const std::string fmt, va_list args) {
-    int size = ((int)fmt.size()) * 2 + 50;   // Use a rubric appropriate for your code
-    std::string str;
-    while (1) {     // Maximum two passes on a POSIX system...
-        str.resize(size);
-        int n = vsnprintf((char *)str.data(), size, fmt.c_str(), args);
-        if (n > -1 && n < size) {  // Everything worked
-            str.resize(n);
-            return str;
-        }
-        if (n > -1)  // Needed size returned
-            size = n + 1;   // For null char
-        else
-            size *= 2;      // Guess at a larger size (OS specific)
-    }
-    return str;
-}
-
 void log_ln(Log_Info info_category, const char * format, ...) {
   va_list args;
   va_start(args, format);
-
+  //printf("\n\n\n%d START PRINT \n\n", pros::millis());
   if (info_category.enabled)
   {
     int size = ((int)strlen(format)) * 2 + 50;   // Initial str size
@@ -202,10 +132,55 @@ void log_ln(Log_Info info_category, const char * format, ...) {
       //printf("   L2 >>> %s \n", new_str.c_str());namespace  {
     }
   }
-
+  //printf("\n\n\n%d DONE PRINT \n\n", pros::millis());
   va_end (args);
 }
 
+/* Buffer -> SD Functions */
+void flush_to_sd(int given_amnt_to_flush, int pos_to_flush_to) {
+  open_log_file();
+  //printf("                  \n  >>>%d Opened file <<< \n\n", pros::millis());
+  if (log_file != NULL) {
+    int amnt_to_flush = std::min(given_amnt_to_flush, MAX_AMNT_TO_FLUSH); // Lim num of indices to flush to MAX_AMNT_TO_FLUSH
+
+    const char* FLUSH_START_POS = &log_buffer[buffer_flush_index];
+    //printf("%d >>>before ToFLUSH: %d | %d %d | \n", pros::millis(), amnt_to_flush, pos_to_flush_to, buffer_flush_index);
+    int flush_amount = fwrite(FLUSH_START_POS, BUF_OBJ_SIZE, amnt_to_flush, log_file);
+    int err = ferror(log_file);
+    //printf("%d >>>after FLUSH %d | %d %d | err: %d\n", pros::millis(), flush_amount,  pos_to_flush_to, buffer_flush_index, ferror(log_file));
+    //int old_bfi = buffer_flush_index;
+    if (flush_amount >  0) buffer_flush_index += flush_amount;
+
+    if (buffer_flush_index >= LOG_BUFFER_SIZE) buffer_flush_index = 0; // If the pointer is at the end of the buffer, move it back to the front
+
+
+    //printf("%d >>>before ToFLUSH: %d | %d %d | \n", pros::millis(), amnt_to_flush, pos_to_flush_to, buffer_flush_index);
+    close_log_file();
+  }
+}
+
+void buffer_to_sd() {
+  printf("  %d PRINT - buffer start \n", pros::millis());
+  log_ln(PROGRAM_FLOW, "%d Start buffer_to_sd() task \n", pros::millis());
+
+  while (true) {
+    int pos_to_flush_to = buffer_write_index; // Take a copy of buffer_write_index (so we task not be affected if log_ln modifies it)
+
+    if (pos_to_flush_to > buffer_flush_index) { // The last character flushed from buf is behind last character written to it
+      int amnt_to_flush = pos_to_flush_to-buffer_flush_index;
+      flush_to_sd(amnt_to_flush, pos_to_flush_to); // Flush everything b/w buffer_flush_index -> pos_to_flush_to
+    }
+    else if (pos_to_flush_to < buffer_flush_index) { // We wrapped around when writing to the buffer (meaning the last character written is behind the last character flushed)
+      printf("  >>%d buffer_to_sd() BUFFER->SD wrapped around \n", pros::millis());
+      int buf_remaining_len = LOG_BUFFER_SIZE - buffer_flush_index; // Amount of unflushed indices left in buffer
+      flush_to_sd(buf_remaining_len, pos_to_flush_to); // Flush b/w buffer_flush_index -> buffer_end. Move the pointer back to the beginning of the buffer
+    }
+
+    pros::delay(LOG_BUFFER_FLUSH_DELAY);
+  }
+}
+
+/* Test Functions */
 void _test_flush_time() {
   constexpr int BUF_TEST_SIZE = 100;
 	char buf_test[BUF_TEST_SIZE];
--- main/src/libraries/logs.hpp
+++ main/src/libraries/logs.hpp
@@ -33,28 +33,32 @@ extern const char* const log_mode; // Logging mode that is used
 extern FILE* log_file; // File on SD card that we print to
 extern pros::Mutex sd_buffer_mutex; // sd_buffer_mutex used to ensure only one piece of code touches the buffer at a time
 constexpr int LOG_BUFFER_FLUSH_DELAY = 500; // Amount of time between consecutive flushes of the buffer into the SD
-constexpr int LOG_MUTEX_TO = 15; // Max amnt of time that log_ln_internal() will wait for sd_buffer_mutex
+constexpr int LOG_MUTEX_TO = 5; // Max amnt of time that log_ln_internal() will wait for sd_buffer_mutex
 constexpr int MAX_ALLOWABLE_OPEN_ATTEMPTS = 100; // Max num of attempts that log_init() will make when trying to open the file
 constexpr int MAX_ALLOWABLE_CLOSE_ATTEMPTS = 100; // Max num of attempts that close_log_file will make when trying to close the file
 
 /* Logging Buffer */
-constexpr int LOG_BUFFER_SIZE = 10000; // Max size of log_buffer
+constexpr int LOG_BUFFER_SIZE = 1000; // Max size of log_buffer
 extern char log_buffer[LOG_BUFFER_SIZE]; // If sd_logging_enabled, log functions log to this buffer. Then, buffer_to_sd() task flushes buffer into SD
 extern int buffer_write_index; // Last index in buffer that was written to + 1 ; Writes to the buffer start from this index
 extern int buffer_flush_index; // Last index in buffer that was flushed onto the SD card ; buffer_to_sd() flushes buffer from this index to buffer_write_index
 constexpr size_t BUF_OBJ_SIZE = sizeof(log_buffer[0]); // Size of single element in the buffer
 
-constexpr int MAX_AMNT_TO_FLUSH = 1000; // Maximum amount of characters allowed to be flushed to the SD at once. Flushing 1000 takes approx. 13ms
+constexpr int MAX_AMNT_TO_FLUSH = LOG_BUFFER_SIZE; // Maximum amount of characters allowed to be flushed to the SD at once. Flushing 1000 takes approx. 13ms
+
+/* Log_File open/close Functions */
+void open_log_file(); // Open log_file and sets log_file ptr. Will attempt to open file MAX_ALLOWABLE_OPEN_ATTEMPTS times
+void close_log_file(); // Closes log_file and sets log_file to NULL. Will attempt to close file MAX_ALLOWABLE_CLOSE_ATTEMPTS times
 
 /* Logging Functions */
-void close_log_file(); // Closes log_file and sets log_file to NULL. Will attempt to close file MAX_ALLOWABLE_CLOSE_ATTEMPTS times
 void log_init(); // Initializes sd logging; Will disabling sd_logging if it cannot open the log_file
 void log_ln_internal(const char * str_whole); // Used by log_ln function overloads to log to console and log_buffer (for sd)
-void flush_to_sd(int given_amnt_to_flush); // Flushes given_amnt_to_flush of indices (capped at MAX_AMNT_TO_FLUSH), to the sd card
-void buffer_to_sd(); // Push new data from the buffer into the sd
-
 void log_ln(Log_Info info_category, const char * format, ...); // Logs single line to SD and console (if info_category.enabled)
 void log_ln(Log_Info info_category, Log_Info info_subsystem, const char * format, ...);  // Logs single line to SD and console (if info_category.enabled AND info_subsystem.enabled)
+
+/* Buffer -> SD Functions */
+void flush_to_sd(int given_amnt_to_flush, int pos_to_flush_to); // Flushes given_amnt_to_flush of indices (capped at MAX_AMNT_TO_FLUSH), to the sd card
+void buffer_to_sd(); // Push new data from the buffer into the SD. Is started as a task in log_init()
 
 /* Test Functions */
 void _test_flush_time(); // Call to test how long it takes to copy an array of a given size.
