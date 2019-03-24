c5c7cb652d4d631173e0cc03f24ae0948f54c52c
anjalee-narenthiren <42633976+anjalee-narenthiren@users.noreply.github.com>
2019-03-24 14:11:09 -0400

    Create display-sensors file

---
+++ display-sensors/src/config.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include "main.h"
+
+extern pilons::Controller master;
+extern pilons::Controller partner;
+
+extern pros::ADIEncoder enc_s;
+extern pros::Motor m_intake;
+extern pros::Motor m_drive_fl;
+extern pros::Motor m_drive_fr;
+extern pros::Motor m_drive_bl;
+extern pros::Motor m_drive_br;
+extern pros::Motor m_angler;
+extern pros::ADIAnalogIn s_pole_poti;
+extern pros::Motor m_puncher;
+extern pros::ADILineSensor s_ball_detector;
+extern pros::Motor m_capper;
